5038451d9c5b1702f352fedd5f87673518fa2570
kudinov <kudinov.mos@gmail.com>
2020-11-11 19:01:43 +0300

    add views music files on post template

--- music_square/settings.py
+++ music_square/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'music_square.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
